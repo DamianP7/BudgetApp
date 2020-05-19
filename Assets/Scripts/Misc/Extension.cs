0b93485223599f99d57345772a3b900a50a8f438
DamianP7 <damian7it@gmail.com>
2020-05-19 19:15:32 +0200

    Imported utils

---
+++ Assets/Scripts/Misc/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Extension
+{
+    public static string ToEuropeanString(this DateTime date)
+    {
+        return date.ToString("dd.MM.yyyy");
+    }
+
+    public static string ToEuropeanShortString(this DateTime date)
+    {
+        return date.ToString("dd.MM.yy");
+    }
+}
---
+++ Assets/Scripts/Misc/Screenshots.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Utils
+{
+	public class Screenshots : MonoBehaviour
+	{
+		public KeyCode screenKey;
+		public string imageName = "ss_";
+
+		int id
+		{
+			get => PlayerPrefs.GetInt("ssId", 0);
+			set => PlayerPrefs.SetInt("ssId", value);
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(screenKey))
+				DoScreenshot();
+		}
+
+		void DoScreenshot()
+		{
+			string name = imageName + string.Format("{0:000}", id);
+			ScreenCapture.CaptureScreenshot(name + ".png");
+			id++;
+
+			Debug.Log(name + " captured");
+		}
+	}
+}
